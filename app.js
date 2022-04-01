f03cc8cd3f0a28ca6d4c2be150f0efa74be5e194
Kaushik Nitin Naik <kaushik.naik1602@gmail.com>
2022-04-01 23:56:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const request =  require('request');
+const chalk = require('chalk');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+
+geocode('Mumbai',(error,data)=>{
+    console.log("Message : ",error," Data : ",data);
+    if(data){
+        forecast(data.lat,data.lon,(err,res)=>{
+            console.log("Message : ",err," Res : ",res)
+        })
+    }
+})
